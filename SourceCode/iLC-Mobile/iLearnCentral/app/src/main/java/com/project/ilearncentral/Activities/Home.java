19f9581b97c31723b0b1132565ee03765e256b0d
zcykeproweed <55561333+zcykeproweed@users.noreply.github.com>
2020-01-25 20:45:11 +0800

    Create Home.java

---
+++ SourceCode/iLC-Mobile/iLearnCentral/app/src/main/java/com/project/ilearncentral/Activities/Home.java
@@ -0,0 +1,56 @@
+package com.project.ilearncentral.Activities;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.project.ilearncentral.R;
+
+public class Home extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout userPageLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu_activity_pages);
+
+        userPageLink = (LinearLayout)findViewById(R.id.user_page_link);
+        userPageLink.setOnClickListener(this);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.user_page_link:
+                onBackPressed();
+            default:
+                return;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_pages, menu);
+        return true;
+    }
+}
