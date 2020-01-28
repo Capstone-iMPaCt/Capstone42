b698c1d5c5e5273ea2d047a9046c9baf9dc765ca
mcjeph <52535123+mcjeph@users.noreply.github.com>
2020-01-28 14:37:29 +0800

    Revert "updates..."
    This reverts commit 06d4472c560ad0eac3f86ab2a8e49eb341c198f4.

--- SourceCode/iLC-Mobile/iLearnCentral/app/src/main/java/com/project/ilearncentral/Activity/NewsFeed.java
+++ SourceCode/iLC-Mobile/iLearnCentral/app/src/main/java/com/project/ilearncentral/Activity/NewsFeed.java
@@ -9,17 +9,17 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.project.ilearncentral.Adapter.PostAdapter;
-import com.project.ilearncentral.Model.Post;
+import com.project.ilearncentral.Adapter.NewsFeedAdapter;
+import com.project.ilearncentral.Model.News;
 import com.project.ilearncentral.R;
 
 import java.util.ArrayList;
 
 public class NewsFeed extends Fragment {
 
-    private PostAdapter adapter;
+    private NewsFeedAdapter adapter;
     private RecyclerView recyclerView;
-    private ArrayList<Post> news;
+    private ArrayList<News> news;
 
     public NewsFeed() {
         // Required empty public constructor
@@ -33,32 +33,32 @@ public class NewsFeed extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_news_feed, container, false);
         // set up the RecyclerView
         news = new ArrayList<>();
-        news.add(new Post(R.drawable.user, "OnePlus 6T Camera Review:", "6 July 1994", "11:50 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.avatar_boy, "I love Programming And Design", "6 July 1994", "11:51 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
-        news.add(new Post(R.drawable.user, "My first trip to Thailand story ", "6 July 1994", "11:52 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.user, "After Facebook Messenger, Viber now gets...", "6 July 1994", "11:50 AM", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
-        news.add(new Post(R.drawable.avatar_boy, "Isometric Design Grid Concept", "6 July 1994", "11:53 AM", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
-        news.add(new Post(R.drawable.user, "Android R Design Concept 4K", "6 July 1994", "11:54 AM", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
+        news.add(new News(R.drawable.user, "OnePlus 6T Camera Review:", "6 July 1994", "11:50 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.avatar_boy, "I love Programming And Design", "6 July 1994", "11:51 A.M", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
+        news.add(new News(R.drawable.user, "My first trip to Thailand story ", "6 July 1994", "11:52 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.user, "After Facebook Messenger, Viber now gets...", "6 July 1994", "11:50 A.M.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
+        news.add(new News(R.drawable.avatar_boy, "Isometric Design Grid Concept", "6 July 1994", "11:53 A.M.", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
+        news.add(new News(R.drawable.user, "Android R Design Concept 4K", "6 July 1994", "11:54 A.M.", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
 
-        news.add(new Post(R.drawable.user, "OnePlus 6T Camera Review:", "7 July 1994", "11:50 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.user, "I love Programming And Design", "7 July 1994", "11:51 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
-        news.add(new Post(R.drawable.avatar_boy, "My first trip to Thailand story ", "7 July 1994", "11:52 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.user, "After Facebook Messenger, Viber now gets...", "7 July 1994", "11:50 AM", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
-        news.add(new Post(R.drawable.user, "Isometric Design Grid Concept", "7 July 1994", "11:53 AM", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
-        news.add(new Post(R.drawable.avatar_boy, "Android R Design Concept 4K", "6 July 1994", "11:54 AM", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
+        news.add(new News(R.drawable.user, "OnePlus 6T Camera Review:", "7 July 1994", "11:50 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.user, "I love Programming And Design", "7 July 1994", "11:51 A.M", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
+        news.add(new News(R.drawable.avatar_boy, "My first trip to Thailand story ", "7 July 1994", "11:52 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.user, "After Facebook Messenger, Viber now gets...", "7 July 1994", "11:50 A.M.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
+        news.add(new News(R.drawable.user, "Isometric Design Grid Concept", "7 July 1994", "11:53 A.M.", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
+        news.add(new News(R.drawable.avatar_boy, "Android R Design Concept 4K", "6 July 1994", "11:54 A.M.", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
 
-        news.add(new Post(R.drawable.avatar_boy, "OnePlus 6T Camera Review:", "8 July 1994", "11:50 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.user, "I love Programming And Design", "8 July 1994", "11:51 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
-        news.add(new Post(R.drawable.user, "My first trip to Thailand story ", "8 July 1994", "11:52 AM", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
-        news.add(new Post(R.drawable.avatar_boy, "After Facebook Messenger, Viber now gets...", "6 July 1994", "11:50 AM", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
-        news.add(new Post(R.drawable.avatar_boy, "Isometric Design Grid Concept", "8 July 1994", "11:53 AM", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
-        news.add(new Post(R.drawable.user, "Android R Design Concept 4K", "8 July 1994", "11:54 AM", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
+        news.add(new News(R.drawable.avatar_boy, "OnePlus 6T Camera Review:", "8 July 1994", "11:50 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.user, "I love Programming And Design", "8 July 1994", "11:51 A.M", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"));
+        news.add(new News(R.drawable.user, "My first trip to Thailand story ", "8 July 1994", "11:52 A.M.", R.drawable.news_image, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News(R.drawable.avatar_boy, "After Facebook Messenger, Viber now gets...", "6 July 1994", "11:50 A.M.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"));
+        news.add(new News(R.drawable.avatar_boy, "Isometric Design Grid Concept", "8 July 1994", "11:53 A.M.", "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit"));
+        news.add(new News(R.drawable.user, "Android R Design Concept 4K", "8 July 1994", "11:54 A.M.", R.drawable.news_image, "lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit "));
         recyclerView = view.findViewById(R.id.news_container_layout);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new PostAdapter(getContext(), news);
+        adapter = new NewsFeedAdapter(getContext(), news);
         recyclerView.setAdapter(adapter);
         return view;
     }
--- SourceCode/iLC-Mobile/iLearnCentral/app/src/main/java/com/project/ilearncentral/Adapter/NewsFeedAdapter.java
+++ SourceCode/iLC-Mobile/iLearnCentral/app/src/main/java/com/project/ilearncentral/Adapter/NewsFeedAdapter.java
@@ -15,25 +15,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.project.ilearncentral.Model.Post;
+import com.project.ilearncentral.Model.News;
 import com.project.ilearncentral.R;
 
 import java.util.List;
 
-public class PostAdapter extends RecyclerView.Adapter<PostAdapter.newsViewHolder> {
+public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.newsViewHolder> {
 
     Context context;
-    List<Post> posts;
+    List<News> news;
 
-    public PostAdapter(Context context, List<Post> posts) {
+    public NewsFeedAdapter(Context context, List<News> news) {
         this.context = context;
-        this.posts = posts;
+        this.news = news;
     }
 
     @NonNull
     @Override
     public newsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_news, parent, false);
         return new newsViewHolder(view);
     }
 
@@ -47,20 +47,20 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.newsViewHolder
 
         holder.newsContentImageView.setAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_transition_animation));
         
-        holder.titleTextView.setText(posts.get(position).getTitleTextView());
-        holder.dateTextView.setText(posts.get(position).getDateTextView());
-        holder.timeTextView.setText(posts.get(position).getTimeTextView());
-        holder.contentTextView.setText(posts.get(position).getContentTextView());
+        holder.titleTextView.setText(news.get(position).getTitleTextView());
+        holder.dateTextView.setText(news.get(position).getDateTextView());
+        holder.timeTextView.setText(news.get(position).getTimeTextView());
+        holder.contentTextView.setText(news.get(position).getContentTextView());
 
-        getImage(holder.newsContentImageView, posts.get(position).getNewsContentImageView());
-        getImage(holder.newsUserImageView, posts.get(position).getNewsUserImageView());
+        getImage(holder.newsContentImageView,news.get(position).getNewsContentImageView(),400);
+        getImage(holder.newsUserImageView,news.get(position).getNewsUserImageView(),600);
 
     }
 
 
     @Override
     public int getItemCount() {
-        return posts.size();
+        return news.size();
     }
 
     public class newsViewHolder extends RecyclerView.ViewHolder {
@@ -73,30 +73,20 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.newsViewHolder
         newsViewHolder(View itemView) {
             super(itemView);
             containerLayout = (RelativeLayout)itemView.findViewById(R.id.container_layout);
-            newsUserImageView = (ImageView)itemView.findViewById(R.id.post_user_image);
-            titleTextView = (TextView)itemView.findViewById(R.id.post_title);
+            newsUserImageView = (ImageView)itemView.findViewById(R.id.news_user_imageview);
+            titleTextView = (TextView)itemView.findViewById(R.id.news_title_textview);
             timestampLayout = (LinearLayout)itemView.findViewById(R.id.timestamp_layout);            
-            dateTextView = (TextView)itemView.findViewById(R.id.post_date);
-            timeTextView = (TextView)itemView.findViewById(R.id.post_time);
-            newsContentImageView = (ImageView)itemView.findViewById(R.id.post_content_image);
-            contentTextView = (TextView)itemView.findViewById(R.id.post_description);
+            dateTextView = (TextView)itemView.findViewById(R.id.news_date_textview);
+            timeTextView = (TextView)itemView.findViewById(R.id.news_time_textview);
+            newsContentImageView = (ImageView)itemView.findViewById(R.id.news_content_imageview);
+            contentTextView = (TextView)itemView.findViewById(R.id.content_textview);
         }
     }
-
-    private void getImage(ImageView newsUserImageView, int newsContentImageView) {
-        Glide
-                .with(context)
-                .load(newsContentImageView)
-                .fitCenter()
-                .apply(new RequestOptions())
-                .into(newsUserImageView);
-    }
-
     private void getImage(ImageView newsUserImageView, int newsContentImageView, int height) {
         Glide
                 .with(context)
                 .load(newsContentImageView)
-                .fitCenter()
+                .centerCrop()
                 .apply(new RequestOptions().override(height))
                 .into(newsUserImageView);
     }
